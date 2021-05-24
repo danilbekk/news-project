5d8f689afb921ab78a3c8ec1db77c6598b10e6aa
danilbek <deni200040@gmail.com>
2021-05-24 14:13:27 +0300

    finish

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+
+const hbs = require('express-handlebars')
+
+const data = require('./data')
+
+const app = express()
+
+app.set('view engine', "handlebars")
+
+app.engine('handlebars', hbs())
+
+//app.use(express.json())
+
+app.get('/', (req,res) =>{
+    
+    res.render('content',{
+        mainNews: data.news.slice(0,2),
+        sideBar: data.news.slice(2,6),
+        headers: data.categories
+    }
+    
+    )
+})
+
+app.get('/categoryId/:id',(req,res) =>{
+ const filtr = data.news.filter(item => item.categoryId === Number(req.params.id))
+ res.render('content', {
+    mainNews: filtr.slice(0,2),
+    sideBar: filtr.slice(2,filtr.length),
+    headers: data.categories
+ })
+})
+
+app.get('/news/:id', (req,res) => {
+  const find = data.news.find(item => item.id === Number(req.params.id))
+  console.log(find);
+  res.render('up', {
+      find,
+      headers: data.categories
+
+  })
+})
+
+
+
+// app.get('/news',(req,res) =>{
+//     res.json(data.news)
+// })
+
+// app.get('/itNews',(req,res) => {
+//     res.render('content',{
+//         id:1
+//     })
+// })
+
+app.listen(3000)
